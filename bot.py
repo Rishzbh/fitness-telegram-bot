f250e2c1b1c97263421b4e3c7342afd5ea9eaa22
Rishabh Joshi <123835814+Rishzbh@users.noreply.github.com>
2026-06-04 00:48:11 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,34 @@
-
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-TOKEN = "8933000059:AAFVTe3rcCtY34aXzQEFW1wleRBdsS20XYs"
+TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = 6071673615
+
+scheduler = AsyncIOScheduler()
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.effective_chat.id
-    await update.message.reply_text(
-        f"Your Chat ID is: {chat_id}"
+    await update.message.reply_text("🏃 Fitness Coach Bot Running")
+
+async def send_walk_reminder(app):
+    await app.bot.send_message(
+        chat_id=CHAT_ID,
+        text="🏃 Time for your 3 km walk!"
     )
+
+async def startup(app):
+    # TEST: sends every minute
+    scheduler.add_job(
+        lambda: app.create_task(send_walk_reminder(app)),
+        "interval",
+        minutes=1
+    )
+
+    scheduler.start()
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+app.post_init = startup
 
 app.run_polling()
