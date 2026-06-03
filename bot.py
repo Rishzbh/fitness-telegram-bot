910dda705fe949a0bdc009c5d43c3cdee6adab94
Rishabh Joshi <123835814+Rishzbh@users.noreply.github.com>
2026-06-04 00:47:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,16 @@
+
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from datetime import datetime
 
 TOKEN = "8933000059:AAFVTe3rcCtY34aXzQEFW1wleRBdsS20XYs"
-
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
     await update.message.reply_text(
-        "🏃 Fitness Coach Bot is running!"
-    )
-
-async def time_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(
-        str(datetime.now())
+        f"Your Chat ID is: {chat_id}"
     )
 
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(CommandHandler("time", time_cmd))
 
 app.run_polling()
